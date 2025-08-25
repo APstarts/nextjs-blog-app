5e68f3491d4c1d6570886eafd78ef1fbed5cebe3
Anurag Pal <tanmaypal123@outlook.com>
2025-08-25 22:46:33 +0530

    initializing

--- auth.ts
+++ auth.ts
@@ -1,7 +1,33 @@
-
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { sql } from "./lib/db"
+
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  callbacks: {
+ async signIn({ profile }) {
+  
+  try {
+    await sql`INSERT INTO users (email, name, image) VALUES (${profile?.email ?? ""}, ${profile?.name ?? ""}, ${profile?.picture ?? null}) 
+    ON CONFLICT (email) DO UPDATE SET name = EXCLUDED.name, image = EXCLUDED.image`
+  } catch (error) {
+    console.error("Error inserting user: ", error);
+  }
+  return true;
+ },
+ async session({session}){
+  if(session?.user?.email){
+    try{
+      const rows = await sql`SELECT id FROM users WHERE email = ${session?.user?.email}`;
+      if(rows.length > 0){
+        (session.user as any).id = rows[0].id;
+      }
+    } catch (err) {
+      console.error("Error fetching user id: ", err);
+    }
+  }
+  return session;
+ }
+}
 })
--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,3 @@
-// lib/db.ts
-import { sql, type QueryResultRow } from "@vercel/postgres";
-
-/**
- * Run parameterized queries with type inference.
- * Example:
- *   const users = await query<{ id: string; email: string }>`
- *     SELECT id, email FROM users;
- *   `;
- */
-export async function query<T extends QueryResultRow = QueryResultRow>(
-  strings: TemplateStringsArray,
-  ...params: any[]
-) {
-  const { rows } = await sql<T>(strings, ...params);
-  return rows;
-}
+import postgres from 'postgres';
+ 
+export const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
