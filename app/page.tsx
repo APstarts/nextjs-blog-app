fcde4216ac260bd82b5e489393bc9cdbe3dc142d
Anurag Pal <tanmaypal123@outlook.com>
2025-09-01 21:23:42 +0530

    improved user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
+export default async function Home() {
+  const session = await auth();
 
-export default function Home() {
+  if(session?.user){
+    return (
+      redirect("/dashboard")
+    )};
   return (
     <>
     <main>
