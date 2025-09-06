3b77ed58edffeac3d7f927801f25776ada917a43
Anurag Pal <tanmaypal123@outlook.com>
2025-09-06 21:51:09 +0530

    updated the myposts page ui

--- app/dashboard/myposts/[id]/page.tsx
+++ app/dashboard/myposts/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 const page = async ({params}: {params: Promise<{id: string}>}) => {
@@ -6,14 +7,15 @@ const page = async ({params}: {params: Promise<{id: string}>}) => {
     const post = await fetch(`${process.env.NEXTAUTH_URL}/api/posts/myposts/${id}`, {cache: 'no-store'});
     if(!post.ok) throw new Error("Failed to fetch posts");
     const posts = await post.json();
-    console.log(posts);
   return (
     <div>
         <h1 className='text-3xl font-extrabold text-center mt-10'>Your Posts</h1>
-        {posts.map((post: { title: string; content: string; id: string; created_at:string }) => (
-            <div key={post.id} className='max-w-3xl h-20 mx-auto mt-10 px-4 flex justify-between items-center border border-gray-300'>
-                <p className='text-2xl font-bold mb-5'>{post.title}</p><p>{new Date(post.created_at).toLocaleDateString()}</p>
+        {posts.map((post: { title: string; content: string; id: string; created_at:string; slug: string }) => (
+            <Link key={post.id} href={`/dashboard/${post.slug}`}>
+            <div className='max-w-3xl mx-auto px-4 py-2 mb-4 flex justify-between items-center border border-gray-300 rounded-lg hover:shadow-lg duration-300'>
+                <p className='text-xl'>{post.title}</p><p>{new Date(post.created_at).toLocaleDateString(undefined, {day: "numeric", month: "long", year: "numeric"})}</p>
             </div>
+            </Link>
         ))}
     </div>
   )
